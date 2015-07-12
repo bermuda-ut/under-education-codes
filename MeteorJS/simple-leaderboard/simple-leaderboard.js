c208d739ee55139d511b11fab5868bced41ad24b
Bermuda_UT <maxisbac@gmail.com>
2015-07-12 21:34:20 +1000

    Update - added items

--- MeteorJS/simple-leaderboard/simple-leaderboard.js
+++ MeteorJS/simple-leaderboard/simple-leaderboard.js
@@ -1,26 +1,46 @@
-PlayerList = new Mongo.Collection("players"); // global
+/*
+ * simeple-leaderboard.js
+ *
+ * Simple Leaderboard + Click Game
+ * Author      : Max Lee
+ * Email       : hoso1312@gmail.com
+ * Last Update : 12/07/15
+ * */
+
+// Global
+PlayerList = new Mongo.Collection("players");
+AutoCost = 500;
+BombCost = 100;
+BombVal = 50;
 
 if (Meteor.isClient) {
   // Client side
   Meteor.subscribe("players");
 
+  /* List Functions */
   Template.listplayer.helpers({
-    player: function() {
-      return this.player
+    shortDate: function() {
+      return this.date.toLocaleString().split(" ")[0];  // Exclude time
     },
 
-    score: function() {
-      return this.score
-    },
-
-    date: function() {
-      return this.date;
+    selected: function() {
+      return Session.get("selectedPlayer") == this.playerId 
+    }
+  });
+  
+  Template.listplayer.events({
+    "click li": function() {
+      Session.set("selectedPlayer", this.playerId);
+      //var sel = Session.get("selectedPlayer");
+      //console.log(sel)
+      // Disabled logging 
     }
   });
 
+  /* Body Functions */
   Template.body.helpers({
     players: function() {
-      return PlayerList.find({}, {sort: {score: -1}});
+      return PlayerList.find({}, {sort: {score: -1, player: 1}});
     },
 
     loggedin: function() {
@@ -28,6 +48,7 @@ if (Meteor.isClient) {
     }
   });
 
+  /* Manually clicking */
   Template.clickgame.events({
     "click .playerclick": function() {
       Meteor.call('addScore', Meteor.userId());
@@ -35,6 +56,61 @@ if (Meteor.isClient) {
     }
   });
 
+  /* Items */
+  Template.itemdisplay.helpers({
+    autonum: function() {
+      return PlayerList.findOne({playerId: Meteor.userId()}).autonum
+    },
+
+    autocost: function() {
+      return AutoCost
+    },
+
+    bombcost: function() {
+      return BombCost
+    },
+
+    bombamount: function() {
+      return BombVal
+    }
+  });
+
+  // Using items
+  Template.itemdisplay.events({
+    "click #buyAuto": function() {
+      var playerdata = PlayerList.findOne({playerId: Meteor.userId()});
+
+      if (playerdata.score >= AutoCost) {
+        Meteor.call("addScore", playerdata.playerId, -AutoCost);
+        Meteor.call("buyAuto", playerdata.playerId);
+      return false
+      }
+    },
+
+    "click #buyBomb": function() {
+      var playerdata = PlayerList.findOne({playerId: Meteor.userId()});
+      var sel = Session.get('selectedPlayer')
+
+      if (playerdata.score >= BombCost && sel) {
+        Meteor.call("addScore", playerdata.playerId, -BombCost);
+        Meteor.call("addScore", sel, -BombVal);
+      }
+    }
+  });
+
+  // Autoclick ticker
+  Template.itemdisplay.created = function() {
+    this.handle = Meteor.setInterval((function() {
+      if (Meteor.userId()){
+        var playerdata = PlayerList.findOne({playerId: Meteor.userId()});
+        if (playerdata.autonum && playerdata.autonum > 0) {
+          Meteor.call('addScore', playerdata.playerId, playerdata.autonum);
+        }
+      }
+    }), 1000);
+  };
+
+  // Account setup. No need Email address
   Accounts.ui.config({
     passwordSignupFields : "USERNAME_ONLY"
   });
@@ -49,15 +125,23 @@ if (Meteor.isServer) {
 };
 
 Meteor.methods({
-
-  addScore: function(id) {
+  addScore: function(id, scr) {
+    /* Add score function. id = player(user) id. scr = score to add.
+     * Can pass negative value to scr.
+     * Default value of scr is 1.*/
+    scr = typeof scr !== 'undefined' ? scr : 1;
     if (PlayerList.findOne({playerId: id})) {
-      PlayerList.update({playerId: id}, {$inc: {score: 1}});
-      //TODO: use findandupdate method
+      PlayerList.update({playerId: id}, {$inc: {score: scr}}); // update exsisting using scr
     } else {
-      PlayerList.insert({playerId: Meteor.userId(), player: Meteor.user().username, score: 1, date: new Date()});
+      // create new document
+      PlayerList.insert({playerId: Meteor.userId(), player: Meteor.user().username, score: 1, date: new Date(), autonum: 0});
     }
-  }
+  },
 
+  buyAuto: function(id) {
+    /* buy autoclick function, id = player(user) id
+     * increases autoclick by 1.*/
+    PlayerList.update({playerId: id}, {$inc: {autonum: 1}})
+  }
 });
 
